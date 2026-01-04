d32e3d7f85f55a81ae115e719470dd7af61b5e8a
Behzad <acabehzad@gmail.com>
2026-01-04 18:51:33 -0500

    Adding city seperator filters to my project

--- filters.py
+++ filters.py
@@ -11,4 +11,16 @@ def sex_sep(rows: list) -> list[list[dict],list[dict]]:
     
     return [male_members,female_members]
 
+def city_sep(rows: list) -> dict[str:list]:
+    city_sep_members = {}
+    for row in rows:
+        if row['city'] in city_sep_members.keys():
+            city_sep_members[row['city']].append(row)
+        else:
+            city_sep_members[row['city']] = []
+            city_sep_members[row['city']].append(row)
+
+    return city_sep_members
+            
+
 
--- main.py
+++ main.py
@@ -4,13 +4,13 @@ BASE_DIR = Path(__file__).resolve().parent
 INPUT_FILE = BASE_DIR/"athletes.csv"
 
 from logic import read_data
-from filters import sex_sep
+from filters import sex_sep, city_sep
 
 fieldnames = read_data(INPUT_FILE)[0]
-rows = read_data(INPUT_FILE)[1]
+all_members = read_data(INPUT_FILE)[1]
 
-[male_members , female_members] = sex_sep(rows)
+[male_members , female_members] = sex_sep(all_members)
 
-print(female_members[0:3])
+city_sep_members = city_sep(all_members)
 
-
+print(city_sep_members['Mashhad'][0:2])
