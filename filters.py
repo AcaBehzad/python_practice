cabe812846eed64e01bd1427016370e9d6cf2490
Behzad <acabehzad@gmail.com>
2026-01-04 20:01:15 -0500

    Enhanced readability in filters and added BMI calculator to my project

--- filters.py
+++ filters.py
@@ -1,9 +1,20 @@
 import csv
 
-def sex_sep(rows: list) -> list[list[dict],list[dict]]:
+def sex_sep(all_members: list) -> list[list[dict],list[dict]]:
+    """
+    This function receives the list of all members in our data base and seperate them
+    into two lists based on their gender.
+    
+    :param rows: it takes all members
+    :type rows: list
+    :return: it returns two lists:
+    1 - Male members
+    2 - Female members
+    :rtype: list[list[dict]]
+    """
     male_members = []
     female_members = []
-    for row in rows:
+    for row in all_members:
         if row['sex'] == 'M':
             male_members.append(row)
         elif row['sex'] == 'F':
@@ -11,9 +22,19 @@ def sex_sep(rows: list) -> list[list[dict],list[dict]]:
     
     return [male_members,female_members]
 
-def city_sep(rows: list) -> dict[str:list]:
+def city_sep(all_members: list) -> dict[str:list]:
+    """
+    This function receives the list of all members in our data base and seperate them
+    based on their hometown.
+    
+    :param rows: All members
+    :type rows: list
+    :return: a dictionary with keys set as cities, and values are the list of those members living 
+    in that city
+    :rtype: dict
+    """
     city_sep_members = {}
-    for row in rows:
+    for row in all_members:
         if row['city'] in city_sep_members.keys():
             city_sep_members[row['city']].append(row)
         else:
--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
 INPUT_FILE = BASE_DIR/"athletes.csv"
 
-from logic import read_data
+from logic import read_data, bmi_calculator
 from filters import sex_sep, city_sep
 
 fieldnames = read_data(INPUT_FILE)[0]
 all_members = read_data(INPUT_FILE)[1]
 [male_members , female_members] = sex_sep(all_members)
 city_sep_members = city_sep(all_members)
+bmi_caled_members = bmi_calculator(all_members)
+
+print(bmi_caled_members[0:2])
